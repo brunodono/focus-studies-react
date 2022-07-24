e62cc0e249603c9f5a20175e28284544bc57d016
brunodono <bruno_bignard@hotmail.com>
2022-07-24 20:18:02 +0100

    Refactured code to convert some class components to function components

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,15 +1,20 @@
 import React from "react";
 import style from './Button.module.scss'
 
-class Button extends React.Component <any, {type?: 'button' | 'submit' | 'reset' | undefined, onClick?: () => void}> {
-    render() {
-        const {type = "button", onClick } = this.props;
-        return (
-            <button onClick={onClick} type={type} className={style.button}>
-                {this.props.children}                
-            </button>
-        )
-    }
+interface Props {
+    type?: 'button' | 'submit' | 'reset' | undefined, onClick?: () => void, children?: React.ReactNode
+}
+
+function Button({ onClick, type, children}: Props) {
+    return(
+    <button 
+    onClick={onClick} 
+    type={type} 
+    className={style.button}
+    >
+    {children}
+    </button>
+    )
 }
 
 export default Button; 
